6a96b2c005f19a7ad68964566f7d9d5245d64cf4
esor001 <jangmipaik@gmail.com>
2023-05-03 03:50:27 -0400

    help

--- functions.js
+++ functions.js
@@ -8,6 +8,8 @@ $(".spring-btn").click(function(){
     $(".tree").toggleClass("bloom");
     $("body").toggleClass("bloom");
     $(".tree-bloom").toggleClass("bloom");
+    $(".petals").toggleClass("bloom");
+
 
   });
 
@@ -36,13 +38,12 @@ $(".spring-btn").click(function(){
     $(".tree").toggleClass("winter");
     $("body").toggleClass("winter");
     $(".tree-winter").toggleClass("winter");
+    $(".snow").toggleClass("winter");
 
   });
 
 
-$( ".draggable" ).draggable({ 
-   snap: true 
-});
+  $( ".drag" ).draggable();
 
   let music = new Audio({
     loop: true,
@@ -57,6 +58,41 @@ $(document).ready(function(){
 
   });
 });
+
+$('.play').click(function(){
+  var $this = $(this);
+  var id = $this.attr('id').replace(/btn/, '');
+  $this.toggleClass('active');
+  if($this.hasClass('active')){
+      $this.text('pause'); 
+      $('audio[id^="sound"]')[id-1].play();        
+  } else {
+      $this.text('play');
+      $('audio[id^="sound"]')[id-1].pause();
+  }
+});
+
+
+TweenLite.set("#container",{perspective:600})
+TweenLite.set("img",{xPercent:"-50%",yPercent:"-50%"})
+
+var total = 60;
+var warp = document.getElementById("container"),	w = window.innerWidth , h = window.innerHeight;
+ 
+ for (i=0; i<total; i++){ 
+   var Div = document.createElement('div');
+   TweenLite.set(Div,{attr:{class:'petals snow'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   warp.appendChild(Div);
+   animm(Div);
+ }
+ 
+ function animm(elm){   
+   TweenMax.to(elm,R(6,15),{y:h+100,ease:Linear.easeNone,repeat:-1,delay:-15});
+   TweenMax.to(elm,R(4,8),{x:'+=100',rotationZ:R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
+   TweenMax.to(elm,R(2,8),{rotationX:R(0,360),rotationY:R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
+ };
+
+function R(min,max) {return min+Math.random()*(max-min)};
 
 
 
